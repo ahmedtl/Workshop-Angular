c8dc7c9e81eb1d5bc8b5fe769b0c16ae582c4ff1
Ahmed Tlili <61381757+ahmedtl@users.noreply.github.com>
2022-10-09 15:00:05 +0100

    S4

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ListProductComponent,
+    FooterComponent,
+    ListUserComponent,
+    HomeComponent,
+    ContactComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
